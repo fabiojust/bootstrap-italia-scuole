969e97661d4db84e9fd482e73e3e230f87683af4
Fabio Just <121680339+fabiojust@users.noreply.github.com>
2025-08-19 17:39:18 +0200

    Update card-contatti.php

--- html/layouts/com_fields/field/card-contatti.php
+++ html/layouts/com_fields/field/card-contatti.php
@@ -45,7 +45,7 @@ endswitch;
 <?php if ($showLabel == 1) : ?>
     <h2 class="h4 <?php echo $labelClass; ?>" <?php echo $idelm; ?>><?php echo htmlentities($label, ENT_QUOTES | ENT_IGNORE, 'UTF-8'); ?> </h2>
 <?php endif; ?>
-<div class="card card-bg card-servizio purplelight rounded mb-5">
+<div class="card card-bg card-servizio purplelight rounded">
     <div class="card-body pb-0">
         <?php if ($prefix) : ?>
             <span class="field-prefix"><?php echo htmlentities($prefix, ENT_QUOTES | ENT_IGNORE, 'UTF-8'); ?></span>
